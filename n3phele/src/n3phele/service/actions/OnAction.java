707070dbdade230c2427f89bdd2a9fe16497d45e
nigel <nigel.cook@hp.com>
2013-03-16 22:41:26 -0600

    agent initalization
    Signed-off-by: nigel <nigel.cook@hp.com>

--- n3phele/src/n3phele/service/actions/OnAction.java
+++ n3phele/src/n3phele/service/actions/OnAction.java
@@ -182,6 +182,7 @@ public class OnAction extends Action {
 		Client client = ClientFactory.create();
 
 		Credential plain = this.clientCredential.decrypt();
+		log.info("onAction "+plain.getAccount()+":"+plain.getSecret());
 		client.addFilter(new HTTPBasicAuthFilter(plain.getAccount(), plain.getSecret()));
 		client.setReadTimeout(30000);
 		client.setConnectTimeout(5000);
