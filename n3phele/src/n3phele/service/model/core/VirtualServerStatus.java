7797b8fabeba9d0372240b941499d5a5bb2a5671
tinhascheibler <tinhascheibler@gmail.com>
2013-04-12 11:29:28 -0300

    Aaand VirtualServerStatus pushed

---
+++ n3phele/src/n3phele/service/model/core/VirtualServerStatus.java
@@ -0,0 +1,12 @@
+package n3phele.service.model.core;
+import javax.xml.bind.annotation.XmlEnum;
+
+/**
+ * 
+ * @author Cristina Scheibler
+ *
+ */
+@XmlEnum(String.class)
+public enum VirtualServerStatus {
+	running, initializing, debug, terminated, pending
+}
