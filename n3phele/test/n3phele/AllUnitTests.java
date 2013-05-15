b197c7759b1f4b545dacd010a2ffa767209c3e94
LucioC <lucio.cossio@gmail.com>
2013-05-15 16:03:48 -0300

    Added new tests.

--- n3phele/test/n3phele/AllUnitTests.java
+++ n3phele/test/n3phele/AllUnitTests.java
@@ -1,5 +1,6 @@
 package n3phele;
 
+import n3phele.process.CloudProcessManagerTest;
 import n3phele.process.CloudProcessTest;
 import n3phele.service.actions.CreateVMActionTest;
 import n3phele.service.actions.NShellActionTest;
@@ -11,7 +12,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({CloudProcessTest.class, ExpressionTest.class, ParserTest.class, CreateVMActionTest.class, NShellActionTest.class })
+@SuiteClasses({CloudProcessTest.class, ExpressionTest.class, ParserTest.class, CreateVMActionTest.class, NShellActionTest.class, CloudProcessManagerTest.class })
 public class AllUnitTests {
 
 }
