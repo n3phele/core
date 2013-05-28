f391cc6659ac68f18969c11983a7ab33b2f48386
tinhascheibler <tinhascheibler@gmail.com>
2013-05-28 14:34:10 -0300

    adding emma

--- n3phele/test/n3phele/AllUnitTests.java
+++ n3phele/test/n3phele/AllUnitTests.java
@@ -14,8 +14,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({CloudProcessTest.class, ExpressionTest.class, ParserTest.class, CreateVMActionTest.class, NShellActionTest.class, CloudProcessManagerTest.class, 
-	CloudProcessWorkloadsTest.class })
+@SuiteClasses({CloudProcessTest.class, ExpressionTest.class, ParserTest.class, CreateVMActionTest.class, NShellActionTest.class, CloudProcessManagerTest.class})
 public class AllUnitTests {
 
 }
