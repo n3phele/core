a6b7d1cf9b9b4dfc18f262cb5d67d80b7b4c5f57
tinhascheibler <tinhascheibler@gmail.com>
2013-07-25 16:01:20 -0300

    AssimilateAction refactor

--- n3phele/src/n3phele/service/actions/AssimilateVMAction.java
+++ n3phele/src/n3phele/service/actions/AssimilateVMAction.java
@@ -1,26 +1,9 @@
 package n3phele.service.actions;
-/**
-* (C) Copyright 2010-2013. Cristina Scheibler. All rights reserved.
-* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-* 
-* Licensed under the terms described in LICENSE file that accompanied this code, (the "License"); you may not use this file
-* except in compliance with the License. 
-* 
-*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on 
-*  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the 
-*  specific language governing permissions and limitations under the License.
-*/
-import java.io.UnsupportedEncodingException;
+
 import java.net.URI;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Map.Entry;
 import java.util.logging.Level;
 
 import javax.ws.rs.core.MediaType;
@@ -29,11 +12,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
+import n3phele.service.actions.AssimilateAction.AssimilateVirtualServerResult;
 import n3phele.service.core.UnprocessableEntityException;
 import n3phele.service.lifecycle.ProcessLifecycle;
 import n3phele.service.lifecycle.ProcessLifecycle.WaitForSignalRequest;
 import n3phele.service.model.Account;
-import n3phele.service.model.Action;
 import n3phele.service.model.Cloud;
 import n3phele.service.model.CloudProcess;
 import n3phele.service.model.Command;
@@ -66,21 +49,30 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.ClientFilter;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 
+/**
+* (C) Copyright 2010-2013. Cristina Scheibler. All rights reserved.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Licensed under the terms described in LICENSE file that accompanied this code, (the "License"); you may not use this file
+* except in compliance with the License. 
+* 
+*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on 
+*  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+*  specific language governing permissions and limitations under the License.
+*/
 
 @EntitySubclass
-@XmlRootElement(name = "AssimilateAction")
-@XmlType(name = "AssimilateAction", propOrder = { "inProgress","failed", "targetIP" })
+@XmlRootElement(name = "AssimilateVMAction")
+@XmlType(name = "AssimilateVMAction", propOrder = { "failed" })
 @Unindex
 @Cache
-public class AssimilateAction extends Action {
-	final protected static java.util.logging.Logger log = java.util.logging.Logger.getLogger(AssimilateAction.class.getName());
+public class AssimilateVMAction extends VMAction{
+	final protected static java.util.logging.Logger log = java.util.logging.Logger.getLogger(AssimilateVMAction.class.getName());
 	@XmlTransient private ActionLogger logger;
-	private ArrayList<String> inProgress = new ArrayList<String>();
-	private HashMap<String, String> childMap = new HashMap<String, String>();
-	private boolean failed = false;
+	private boolean failed = false;	
+	
 	private String targetIP;
 	
-		
 	@Override
 	public void init() throws Exception {
 		logger = new ActionLogger(this);
@@ -157,17 +149,13 @@ public class AssimilateAction extends Action {
 				URI[] list = response.getRefs();	
 				this.context.putValue("vmFactory", list[0].toString());
 				
+				
 				Context childContext = new Context();
 				childContext.putAll(this.context);
-				childContext.putValue("name", name);
-				
-				CloudProcess children = processLifecycle().spawn(this.getOwner(), childContext.getValue("name"), 
-						childContext, null, this.getProcess(), "AssimilateVM");
-				URI siblingProcess = children.getUri();
-				childMap.put(list[0].toString().replace('.', '_'), siblingProcess.toString());
-				this.inProgress.add(siblingProcess.toString());
+				childContext.putValue("name", name);				
 				
 				
+				CloudProcess children = CloudProcessResource.dao.load(this.getProcess());	
 				
 				try{
 				VirtualServer vs = fetchVirtualServer(client, list[0]);
@@ -176,10 +164,9 @@ public class AssimilateAction extends Action {
 					double value = getValueByCDN(cloud, vs.getParameters());
 					Date date = vs.getCreated();
 					
-					setCloudProcessPrice(account,children,value, date);
+					setCloudProcessPrice(account,actionProcess,value, date);
 					log.info("Process created and set");
 					logger.info("Process created and set");
-					processLifecycle().init(children);
 				}
 				}catch(Exception e) {
 					failed = true;
@@ -200,15 +187,126 @@ public class AssimilateAction extends Action {
 	
 	@Override
 	public boolean call() throws WaitForSignalRequest, Exception {
-		int myChildren = this.inProgress.size();
-		log.info("waiting for children "+myChildren);
-		if(myChildren != 0) {
+		if(epoch != 0) {
+							
+			URI accountURI = Helpers.stringToURI(this.context.getValue("account"));
+			if(accountURI == null)
+				throw new IllegalArgumentException("Missing account");
+			Account account = AccountResource.dao.load(accountURI, this.getOwner());
+			Cloud cloud = CloudResource.dao.load(account.getCloud(), this.getOwner());
+			Client client = ClientFactory.create();
+			ClientFilter factoryAuth = new HTTPBasicAuthFilter(Credential.unencrypted(cloud.getFactoryCredential()).getAccount(), Credential.unencrypted(cloud.getFactoryCredential()).getSecret());
+			client.addFilter(factoryAuth);
+			client.setReadTimeout(90000);
+			client.setConnectTimeout(5000);		
+			
+			
+			try {
+
+				if(this.context.getValue("vmFactory")==null)retrieveVirtualServer(cloud, account);
+				if(this.context.getValue("vmFactory")!=null){
+				if(!this.context.containsKey("publicIpAddress")) {						
+						VirtualServer vs = fetchVirtualServer(client, new URI(this.context.getValue("vmFactory")));						
+						log.info("VMAction: Server status is "+vs.getStatus());
+						URI notification = UriBuilder.fromUri(this.getProcess()).scheme("http").path("event").build();
+						if(!notification.equals(vs.getNotification())) {
+							// FIXME: Update factory to accept notification put .. resource.put(notification);
+						}
+						if(vs.getStatus().equals(VirtualServerStatus.running) && vs.getOutputParameters() != null) {
+							for(NameValue p : vs.getOutputParameters()) {
+								if(p.getKey().equals("publicIpAddress")) {
+									this.context.putValue("publicIpAddress", p.getValue());
+									log.info("VMAction: publicURI is "+p.getValue());
+								} else {
+									this.context.putValue(p.getKey(), p.getValue());
+								}
+							}
+						} else if(vs.getStatus().equals(VirtualServerStatus.terminated)) {
+							logger.error("Client "+this.context.getValue("vmFactory")+" unexpected death.");
+							log.severe("Client "+this.context.getValue("vmFactory")+" unexpected death.");
+							throw new UnprocessableEntityException("Client "+this.context.getValue("vmFactory")+" unexpected death.");
+						} 
+					}
+				
+				epoch = 0;
+				CloudProcess actionProcess = CloudProcessResource.dao.load(this.getProcess());	
+				ProcessLifecycle.mgr().signalParent(this.getProcess(), SignalKind.Ok, this.getProcess().toString());
+				throw new ProcessLifecycle.WaitForSignalRequest();
+				}
+					
+			} catch (UniformInterfaceException e) {
+				ClientResponse response = e.getResponse();
+				if(response != null) {
+					if(response.getStatus() == 404) {
+						logger.error(this.context.getValue("vmFactory")+" unexpected death.");
+						log.severe("Client "+this.context.getValue("vmFactory")+" unexpected death.");
+						throw new UnprocessableEntityException("Client "+this.context.getValue("vmFactory")+" unexpected death.");
+					}
+				}
+			}  catch (ProcessLifecycle.WaitForSignalRequest wait) {
+				throw wait;
+			} catch (Exception e) {
+				log.log(Level.SEVERE, "Unexpected exception", e);
+			} finally {
+				ClientFactory.give(client);
+			}
+			return false; // keep waiting
+		} else {
+			/*
+			 * On going monitoring of the VM
+			 */
+				
+			URI accountURI = Helpers.stringToURI(this.context.getValue("account"));
+			if(accountURI == null)
+				throw new IllegalArgumentException("Missing account");
+			Account account = AccountResource.dao.load(accountURI, this.getOwner());
+			Cloud cloud = CloudResource.dao.load(account.getCloud(), this.getOwner());
+			Client client = ClientFactory.create();
+			ClientFilter factoryAuth = new HTTPBasicAuthFilter(Credential.unencrypted(cloud.getFactoryCredential()).getAccount(), Credential.unencrypted(cloud.getFactoryCredential()).getSecret());
+			client.addFilter(factoryAuth);
+			client.setReadTimeout(90000);
+			client.setConnectTimeout(5000);		
+			VirtualServer vs = null;
+			try {	
+				vs = fetchVirtualServer(client, new URI(this.context.getValue("vmFactory")));		
+			} catch (UniformInterfaceException e) {
+				ClientResponse response = e.getResponse();
+				if(response != null) {
+					if(response.getStatus() == 404) {
+						logger.error(this.context.getValue("vmFactory")+" unexpected death.");
+						log.severe("Client "+this.context.getValue("vmFactory")+" unexpected death.");
+						throw new UnprocessableEntityException("Client "+this.context.getValue("vmFactory")+" unexpected death.");
+					}
+				}
+
+				logger.info("Access error to "+this.context.getValue("vmFactory"));
+				log.log(Level.INFO, "Access error to "+this.context.getValue("vmFactory"), e);
+				throw new ProcessLifecycle.WaitForSignalRequest();
+			} catch (ClientHandlerException e) {
+				logger.info("Access error to "+this.context.getValue("vmFactory"));
+				log.log(Level.INFO, "Access error to "+this.context.getValue("vmFactory"), e);
+			} catch (Exception e) {
+				log.log(Level.SEVERE, "Unexpected exception", e);
+			} finally {
+				ClientFactory.give(client);
+			}
+			
+			log.info("VM "+this.context.getValue("vmFactory")+" "+(vs==null?"disappeared":vs.getName()+":"+vs.getStatus()));
+			if(vs != null && vs.getStatus().equals(VirtualServerStatus.running) && vs.getOutputParameters() != null) {
+				/*
+				 * All is well
+				 */
+			} else if(vs == null || vs.getStatus().equals(VirtualServerStatus.terminated)) {
+				/*
+				 * The VM has died
+				 */
+				logger.error(this.context.getValue("vmFactory")+" unexpected death.");
+				log.severe("Client "+this.context.getValue("vmFactory")+" unexpected death.");
+				throw new UnprocessableEntityException("Client "+this.context.getValue("vmFactory")+" unexpected death.");
+			} 
+					
 			throw new ProcessLifecycle.WaitForSignalRequest();
-		}
-		if(failed) {
-			throw new UnprocessableEntityException("assimilation failed");
-		}
-		return true;
+		}	
 	}
 	@Override
 	public void cancel() {
@@ -226,85 +324,7 @@ public class AssimilateAction extends Action {
 
 	@Override
 	public void signal(SignalKind kind, String assertion) {
-		log.info("Assimilate gets Signal "+kind+":"+assertion);
-		boolean isChild = this.inProgress.contains(assertion);
-		switch(kind) {
-		case Adoption:
-			log.info((isChild?"Child ":"Unknown ")+assertion+" adoption");
-			URI processURI = URI.create(assertion);
-			processLifecycle().dump(processURI);
-			return;
-		case Cancel:
-		case Dump:
-			log.info((isChild?"Child ":"Unknown ")+assertion+" cancelled or dumped");
-			if(isChild) {
-				// FIXME: Not sure if the failure semantics belong here
-				// FIXME: Of if there are parameterized..
-				// FIXME: --onFailure: killAll
-				// FIXME: --onFailure: continue
-				this.inProgress.remove(assertion);
-				this.killVM();
-				failed = true;
-			}
-			break;
-		case Event:
-			if(isChild) {
-				this.inProgress.remove(assertion);
-			} else if(assertion.equals("killVM")) {
-				killVM();
-			} else {
-				Map<String,String> params = parsetoMap(URI.create(assertion));
-				String source = params.get("source");
-				URI process = null;
-				if(source != null) {
-					process = Helpers.stringToURI(childMap.get(source.replace('.', '_')));
-					params.remove("source");
-					String childParams = null;
-					for(Entry<String, String> i : params.entrySet()) {
-						try {
-							if(!i.getKey().equals("oldStatus") && !i.getKey().equals("newStatus"))
-								continue;
-							String name = URLEncoder.encode(i.getKey(), "UTF-8");
-							String value = URLEncoder.encode(i.getValue(), "UTF-8");
-							String fragment = name+"="+value;
-							if(childParams != null) {
-								childParams += "&"+fragment;
-							} else {
-								childParams = fragment;
-							}
-						} catch (UnsupportedEncodingException e) {
-							log.log(Level.SEVERE, "Refactor exception", e);
-						}
-					}
-					if(childParams != null)
-						childParams = "?"+childParams;
-					else
-						childParams = "";
-					URI childAssertion = URI.create(process.toString()+childParams);
-					ProcessLifecycle.mgr().signal(process,SignalKind.Event, childAssertion.toString());
-					log.info("Forward event "+childAssertion+" to "+process);
-				}  else {
-					log.warning("Ignoring event "+assertion);
-				}
-			}
-			return;
-		case Failed:
-			log.info((isChild?"Child ":"Unknown ")+assertion+" failed");
-			if(isChild) {
-				this.inProgress.remove(assertion);
-				this.killVM();
-				failed = true;
-			}
-			break;
-		case Ok:
-			log.info((isChild?"Child ":"Unknown ")+assertion+" ok");
-			if(isChild) {
-				this.inProgress.remove(assertion);
-			}
-			break;
-		default:
-			return;		
-		}
+		log.warning("Signal");
 		
 	}
 
@@ -365,19 +385,19 @@ public class AssimilateAction extends Action {
 		this.failed = failed;
 	}
 	
+	
 	/**
-	 * @return the inProgress
+	 * @return the failed
 	 */
-	public ArrayList<String> getInProgress() {
-		return inProgress;
+	public boolean isFailed() {
+		return this.failed;
 	}
-
-
+	
 	/**
-	 * @param inProgress the inProgress to set
+	 * @return the failed
 	 */
-	public void setInProgress(ArrayList<String> inProgress) {
-		this.inProgress = inProgress;
+	public boolean getFailed() {
+		return this.failed;
 	}
 
 	public void killVM() {
@@ -413,32 +433,6 @@ public class AssimilateAction extends Action {
 			ClientFactory.give(client);
 		}
 	}
-	
-	private Map <String,String> parsetoMap (URI uri) {
-		Map<String,String> result = new HashMap<String,String>();
-		String query = uri.getRawQuery();
-		if(query != null && !query.isEmpty()) {
-			Scanner scanner = new Scanner(uri.getRawQuery());
-		    scanner.useDelimiter("&");
-		    while (scanner.hasNext()) {
-		        String[] nameValue = scanner.next().split("=");
-		        if (nameValue.length == 0 || nameValue.length > 2)
-		            throw new IllegalArgumentException("bad parameter");
-		
-		        try {
-					String name = URLDecoder.decode(nameValue[0], "UTF-8");
-					String value = null;
-					if (nameValue.length == 2)
-					    value = URLDecoder.decode(nameValue[1], "UTF-8");
-					result.put(name, value);
-				} catch (UnsupportedEncodingException e) {
-					log.log(Level.SEVERE, "Parse exception", e);
-				}
-		    }
-		}
-        return result;
-    }
-
 	
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
