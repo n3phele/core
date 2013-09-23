24cf2e885ddb26afac1205033eef308ec84113e6
LucioC <lucio.cossio@gmail.com>
2013-09-23 10:43:33 -0300

    Added copyright notice to new created file.

--- n3phele/src/n3phele/service/model/ProcessCachingAbstractManager.java
+++ n3phele/src/n3phele/service/model/ProcessCachingAbstractManager.java
@@ -1,14 +1,22 @@
+/**
+ * (C) Copyright 2010-2013. Nigel Cook. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * Licensed under the terms described in LICENSE file that accompanied this code, (the "License"); you may not use this file
+ * except in compliance with the License. 
+ * 
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on 
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ *  specific language governing permissions and limitations under the License.
+ *  
+ *  @author Lucio P. Cossio
+ */
 package n3phele.service.model;
-
 import n3phele.service.core.NotFoundException;
 import n3phele.service.model.core.AbstractManager;
 
 public abstract class ProcessCachingAbstractManager extends AbstractManager<CloudProcess> {
-	/** Add a new item to the persistent data store. The item will be updated with a unique key, as well
-	 * the item URI will be updated to include that defined unique team.
-	 * @param item to be added
-	 * @throws IllegalArgumentException for a null argument
-	 */
+
 	@Override
 	protected void add(CloudProcess item) throws IllegalArgumentException {
 			super.add(item);
@@ -26,10 +34,7 @@ public abstract class ProcessCachingAbstractManager extends AbstractManager<Clou
 				ChangeManager.factory().addChange(item.getParent());				
 			}
 	}
-		
-	/** Update a particular object in the persistent data store
-	 * @param item the item to update
-	 */
+
 	@Override
 	protected void update(CloudProcess item)throws NotFoundException {
 		try {
@@ -50,11 +55,7 @@ public abstract class ProcessCachingAbstractManager extends AbstractManager<Clou
 			ChangeManager.factory().addChange(item.getParent());
 		}
 	}
-	
-	/**
-	 * Delete item from the persistent store
-	 * @param item to be deleted
-	 */
+
 	@Override
 	protected void delete(CloudProcess item) {
 		if(item != null) {
