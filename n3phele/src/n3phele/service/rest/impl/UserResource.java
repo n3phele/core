b1a5f4a95e35ceab605ad857e1a2b19ea2f38a69
Leonardo Rosa Amado <leonardo.amado@acad.pucrs.br>
2013-11-27 10:48:59 -0200

    Fixed some issues.

--- n3phele/src/n3phele/service/rest/impl/UserResource.java
+++ n3phele/src/n3phele/service/rest/impl/UserResource.java
@@ -107,7 +107,7 @@ public class UserResource {
 			@FormParam("accountId") String accountId,
 			@FormParam("accountSecret") String accountSecret) {
 		if(email == null || !email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")) {
-			throw new IllegalArgumentException("bad user");		
+			throw new IllegalArgumentException("bad email");		
 		}
 		if(firstName == null || firstName.trim().length() == 0)
 			throw new IllegalArgumentException("bad firstName");
